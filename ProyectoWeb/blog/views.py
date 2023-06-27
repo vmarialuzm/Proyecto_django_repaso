539183ad71338601bbfe987510db26e9eedce687
vmarialuzm <vmarialuzm@gmail.com>
2023-06-27 11:22:13 -0500

    funcionalidad a categorias

--- ProyectoWeb/blog/views.py
+++ ProyectoWeb/blog/views.py
@@ -4,3 +4,8 @@ from .models import Categoria, Post
 def blog(request):
     posts = Post.objects.all()
     return render(request,'blog/blog.html',{"posts":posts})
+
+def categoria(request,categoria_id):
+    categoria = Categoria.objects.get(id=categoria_id)
+    posts = Post.objects.filter(categorias=categoria)
+    return render(request,'blog/categoria.html',{"categoria":categoria,"posts":posts})
