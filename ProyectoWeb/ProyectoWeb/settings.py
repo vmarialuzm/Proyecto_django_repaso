ad75d9461ad54069e39139b05b5d149877442365
vmarialuzm <vmarialuzm@gmail.com>
2023-06-28 20:32:49 -0500

    crear carro de compre

--- ProyectoWeb/ProyectoWeb/settings.py
+++ ProyectoWeb/ProyectoWeb/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'contacto.apps.ContactoConfig',
     'tienda.apps.TiendaConfig',
+    'carro.apps.CarroConfig',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'carro.context_processor.importe_total_carro',
             ],
         },
     },
